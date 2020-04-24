fb538e212f01c9a3c8bf4805c14694fff87f34d6
hahuyhoang411 <64120343+hahuyhoang411@users.noreply.github.com>
2020-04-24 18:04:51 +0700

    Add files via upload

--- UTV.R
+++ UTV.R
@@ -17,20 +17,29 @@ library(psych)
 
 data = filter(data, breast.quad != '?' & node.caps != '?')
 
-#do thi
-p1=ggplot(data, aes(age)) + geom_bar(aes(fill=age))+labs(title = "Age")
-p2=ggplot(data, aes(Class)) +geom_bar(aes(fill=Class))+labs(title = "Class")
-p3=ggplot(data, aes(irradiat)) + geom_bar(aes(irradiat))+labs(title = "irradiat")
-p4=ggplot(data, aes(breast.quad)) + geom_bar(aes(fill=breast.quad))+labs(title = "breast.quad")
-p5=ggplot(data, aes(breast)) + geom_bar(aes(fill=breast))+labs(title = "breast")
-p6=ggplot(data, aes(node.caps)) + geom_bar(aes(fill=node.caps))+ labs(title = "node.caps")
-p7=ggplot(data, aes(menopause)) + geom_bar(aes(fill=menopause))+labs(title = "menopause")
-p8=ggplot(data, aes(tumor.size)) + geom_bar(aes(fill=tumor.size))+labs(title = "Tumor Size")
-p9=ggplot(data, aes(inv.nodes)) + geom_bar(aes(fill=inv.nodes))+labs(title = "inv nodes")
+#Relationship between colums
+ggplot(data, aes(age)) + geom_bar(aes(fill=age))+labs(title = "Age")
+p2 = ggplot(data, aes(Class)) +geom_bar(aes(fill=Class))+labs(title = "Class")
+p3= ggplot(data, aes(irradiat)) + geom_bar(aes(fill=irradiat))+labs(title = "irradiat")
+p4 = ggplot(data, aes(breast.quad)) + geom_bar(aes(fill=breast.quad))+labs(title = "breast.quad")
+p5 = ggplot(data, aes(breast)) + geom_bar(aes(fill=breast))+labs(title = "breast")
+p6 = ggplot(data, aes(node.caps)) + geom_bar(aes(fill=node.caps))+ labs(title = "node.caps")
+p7 = ggplot(data, aes(menopause)) + geom_bar(aes(fill=menopause))+labs(title = "menopause")
+p8 = ggplot(data, aes(tumor.size)) + geom_bar(aes(fill=tumor.size))+labs(title = "Tumor Size")
+p9 = ggplot(data, aes(inv.nodes)) + geom_bar(aes(fill=inv.nodes))+labs(title = "inv nodes")
 grid.arrange(p1,p2,p3,p4,p5,p6,p7,p8,p9)
 
+#Sidebyside barchart
+ggplot(data=data,aes(x=age,fill=irradiat,))+geom_bar()
+
+#Connect
+ggplot(data,aes(tumor.size))+geom_bar()+facet_wrap(~irradiat)+theme(axis.text.x = element_text(angle = 90))+ggtitle("Irradiation Based on Tumor Size")+xlab("Tumor Size")+ylab("Count")
+ggplot(data,aes(x=Class,fill=irradiat))+geom_bar(position ="dodge")
+ggplot(data,aes(x=tumor.size,fill=menopause))+geom_bar(position="dodge")
+ggplot(data,aes(x=age,fill=tumor.size))+geom_bar(position ="dodge")
+ggplot(data,aes(x="",fill=menopause)) + geom_bar(width=3)+coord_polar("y")
 #density plot - ki
-ggplot(data,aes(x=data$`deg.malig`,fill=`tumor.size`))+geom_density(alpha=0.4)+ggtitle("Deg Malignant vs Tumor size")+xlab("Deg Malignant")+ylab("Density")
+ggplot(data,aes(x=`deg.malig`,fill=`tumor.size`))+geom_density(alpha=0.4)+ggtitle("Deg Malignant vs Tumor size")+xlab("Deg Malignant")+ylab("Density")
 
 sum(is.na(data))
 #Fix NA cua nodecaps
@@ -68,9 +77,10 @@ describe(data)
 
 #Testing
 pairs.panels(data)
-t.test(as.numeric(data$age)~data$irradiat)
-ANOVA = aov(as.numeric(data$menopause)~data$node.caps, data=data)
-ANOVA
+t.test(as.numeric(data$tumor.size)~data$irradiat)
+t.test(as.numeric(data$tumor.size)~data$node.caps)
+ANOVA = aov(as.numeric(data$tumor.size)~data$node.caps, data=data)
+summary(ANOVA)
 TukeyHSD(ANOVA)
 
 #model
@@ -80,3 +90,12 @@ data$deg.malig=factor(data$deg.malig,levels=c(1,2,3))
 set.seed(1000)
 shuf_ind = sample(1:nrow(log_data))
 log_data = log_data[shuf_ind,]
+
+model <- lm(as.numeric(data$tumor.size) ~ ., data=data)
+step.model <- stepAIC(model, direction = "both", 
+                      trace = FALSE)
+summary(step.model)
+predict(step.model)
+op = par(mfrow = c(2,2))
+p12 = plot(step.model, c(1,2))
+p13 = spreadLevelPlot(step.model)
